1419bf144632c0a1c44a73b7525a5c156d5a73c5
Beatrix <49019844+DivineBee@users.noreply.github.com>
2020-11-24 21:02:55 +0200

    Added registration with login/logout and encryption of password(Bcrypt) in DB.

--- src/main/java/com/pbl/filesharing/FileSharing/controller/AppController.java
+++ src/main/java/com/pbl/filesharing/FileSharing/controller/AppController.java
@@ -1,9 +1,12 @@
 package com.pbl.filesharing.FileSharing.controller;
 
 import com.pbl.filesharing.FileSharing.entity.Document;
+import com.pbl.filesharing.FileSharing.entity.User;
 import com.pbl.filesharing.FileSharing.repository.DocumentRepository;
+import com.pbl.filesharing.FileSharing.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -29,14 +32,16 @@ import java.util.Optional;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private DocumentRepository repository;
 
     @GetMapping("/")
-    public String viewHomePage(Model model){
-        List<Document> listDocs = repository.findAll();
-        model.addAttribute("listDocs", listDocs);
-        return "home";
+    public String viewHomePage(){
+        return "index";
     }
 
     @PostMapping("/upload")
@@ -75,5 +80,29 @@ public class AppController {
         ServletOutputStream outputStream = response.getOutputStream();
         outputStream.write(document.getContent());
         outputStream.close();
+    }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model){
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        return "register_success";
+    }
+
+    @GetMapping("/home")
+    public String viewDocuments(Model model){
+        List<Document> listDocs = repository.findAll();
+        model.addAttribute("listDocs", listDocs);
+        return "home";
     }
 }
